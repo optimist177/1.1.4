1d511aeed83cdac5010a12f0cecd7f6b93b0526b
Vladimir Nekrasov <nve4@yandex.ru>
2022-11-07 13:49:48 +0300

    test3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,14 +2,16 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-
+    private Transaction transaction;
     private final SessionFactory factory = Util.getFactory();
 
     public UserDaoHibernateImpl() {
@@ -20,7 +22,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
         try (Session session = factory.getCurrentSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createSQLQuery("CREATE TABLE if NOT EXISTS `users` (\n" +
                     "  `id` int NOT NULL AUTO_INCREMENT,\n" +
                     "  `name` varchar(45) NOT NULL,\n" +
@@ -29,6 +31,10 @@ public class UserDaoHibernateImpl implements UserDao {
                     "  PRIMARY KEY (`id`)\n" +
                     ") ").executeUpdate();
             session.getTransaction().commit();
+        }catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
@@ -36,9 +42,13 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         try (Session session = factory.getCurrentSession()){
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createSQLQuery("DROP TABLE if EXISTS users").executeUpdate();
             session.getTransaction().commit();
+        }catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
@@ -46,26 +56,34 @@ public class UserDaoHibernateImpl implements UserDao {
     public void saveUser(String name, String lastName, byte age) {
         try (Session session = factory.getCurrentSession()) {
             User user = new User(name, lastName, age);
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
+        }catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public void removeUserById(long id) {
         try (Session session = factory.getCurrentSession()){
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             User user = session.get(User.class, id);
             session.delete(user);
             session.getTransaction().commit();
+        }catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public List<User> getAllUsers() {
         try (Session session = factory.getCurrentSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             List<User> userList = session.createQuery("FROM User", User.class).getResultList();
             return userList;
         }
@@ -74,9 +92,13 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void cleanUsersTable() {
         try(Session session = factory.getCurrentSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createQuery("delete User").executeUpdate();
             session.getTransaction().commit();
+        }catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 }
